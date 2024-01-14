6179f9d162d7326c57652847ea8f0b5985e016de
jaimanm <jaimancoding@gmail.com>
2024-01-14 12:14:20 -0500

    accounted for invalid line numbers

--- app/prophecy/prophecy.tsx
+++ app/prophecy/prophecy.tsx
@@ -1,53 +1,3 @@
-// import { readFileSync } from 'fs';
-
-// export function Prophecy({ book, lineNumber }) {
-//     // const book = getRandomNumber(1, 12)
-//     // const bookLength = readFileSync('public/Aeneid_Latin/Aeneid_Latin_' + book + '.txt', 'utf-8').split('\n').length
-//     // const lineNumber = getRandomNumber(1, bookLength)
-//     const numLines = 3
-//     const latinLine = getLatin(book, lineNumber, numLines)
-//     const englishLine = getEnglish(book, lineNumber, numLines)
-
-//     return (
-//         <div className='flex-col justify-center text-2xl mx-16'>
-//             <p className='italic text-xl'>Aeneid Book {book}, Lines {lineNumber - 3}-{lineNumber}</p>
-//             <br></br>
-//             <p>Latin:</p>
-//             <ul>
-//                 {latinLine.map((line, index) => (
-//                     <li key={index}>{line}</li>
-//                 ))}
-//             </ul>
-//             <br></br>
-//             <p className=''>English:</p>
-//             <ul>
-//                 {englishLine.map((line, index) => (
-//                     <li key={index}>{line}</li>
-//                 ))}
-//             </ul>
-//         </div>
-//     )
-// }
-
-
-// function getLatin(book, lineNumber, numLines) {
-//     // get a random Latin line from the Aeneid
-//     // the .txt files are in ../../lib/
-//     var lines = readFileSync('public/Aeneid_Latin/Aeneid_Latin_' + book + '.txt', 'utf-8').split('\n')
-
-//     // return a random line
-//     var line = lines.slice((lineNumber - 1) - numLines, (lineNumber - 1))
-//     return line
-// }
-// function getEnglish(book, lineNumber, numLines) {
-//     // get a random English line from the Aeneid
-//     // the .txt files are in ../../lib/
-//     var lines = readFileSync('public/Aeneid_English/Aeneid_English_' + book + '.txt', 'utf-8').split('\n')
-    
-//     // return a random line
-//     var line = lines.slice((lineNumber - 1) - numLines, (lineNumber - 1))
-//     return line
-// }
 'use client'
 
 import { useState, useEffect } from 'react';
@@ -75,7 +25,13 @@ export function Prophecy() {
             .then(response => response.text())
             .then(data => {
                 const lines = data.split('\n');
-                setLineNumber(getRandomNumber(1, lines.length));
+                setLineNumber(() => {
+                    var randLine = getRandomNumber(1, lines.length);
+                    while (randLine - numLines <= 1) {
+                        randLine = getRandomNumber(1, lines.length);
+                    }
+                    return randLine;
+                });
                 const line = lines.slice((lineNumber - 1) - numLines, (lineNumber - 1));
                 setLatinLine(line);
             });
